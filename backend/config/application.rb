78fd2ab2d67c1745f11bfc23b9c9640f40179c8b
fujisawa <rush2065kingcrab0120@gmail.com>
2023-12-08 18:45:45 +0900

    test:rspec導入予定なので、自動生成されるtestファイル設定をfalseに変更

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -36,5 +36,8 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.generators do |g|
+      g.test_framework false #test以下にファイルを作成しない。
+    end
   end
 end
